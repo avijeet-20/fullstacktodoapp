19f59ad2db06d4f09417dd21c53fad5d1cc9da13
avijeet-20 <avi.pandey.20@gmail.com>
2024-02-12 15:15:19 +0530

    initialised express boilerplate

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/todo', (req,res) => {
+
+})
+
+app.post('/todo', (req,res) => {
+
+})
+
+app.put('/todo', (req,res) => {
+
+})
+
+app.listen(3000);
--- readme.md
+++
@@ -1,3 +0,0 @@
-#full stack to do application 
-
--full stack application using express.js for backend and react for front end
